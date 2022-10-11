48c23253e1e3e83264cd36308402a2a19460a52d
liuxiangxiang <liuxiangxiang1992@gmail.com>
2022-10-11 22:52:06 +0800

    add：test

--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -12,6 +12,9 @@ import { ref } from "vue"
 const number1 = ref<number>(1)
 console.log(number1);
 
+const aa = ref<boolean>(false);
+
+
 let tup1: [string, number, boolean]
 tup1 = ['小明', 12, false]
 
